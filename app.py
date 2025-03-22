59ad58b1ce06e31fff6ffc99c951eca6103b16ab
Gautam-kumar9 <231212013@nitdelhi.ac.in>
2025-03-22 11:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 # Download necessary NLTK datasets
-nltk.download('punkt')
+# nltk.download('punkt')
+import nltk
+import os
+
+# Set custom NLTK data path
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+# Download required datasets
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+
 nltk.download('stopwords')
 
 ps = PorterStemmer()
